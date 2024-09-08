01ac38261d115318eface0b4f76ed82a86216fa3
lsh <3040114965@qq.com>
2024-09-08 17:15:58 +0800

    init

---
+++ src/main/java/org/lsh/Main.java
@@ -0,0 +1,10 @@
+package org.lsh;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    public static native int sum(int a, int b);
+}
