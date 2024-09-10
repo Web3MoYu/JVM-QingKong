52e6ec9078e9e0017e773d4316405f7b9dc0ce1e
lsh <3040114965@qq.com>
2024-09-10 17:01:55 +0800

    1

---
+++ src/main/java/org/lsh/three/Test1.java
@@ -0,0 +1,16 @@
+package org.lsh.three;
+
+public class Test1 {
+
+    public static int t = 10;
+
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println(i);
+        int a = i++;
+        System.out.println(i);
+        int b = ++i;
+        System.out.println(i);
+
+    }
+}
